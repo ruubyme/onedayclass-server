ab0d0b6c9bfd23e7621b7292188507ac4f9f9cdb
ruubyme <wngml1741@naver.com>
2024-01-26 10:58:39 +0900

    test

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "https://todo-list-typescript-git-feat-todo-edit-ruubyme.vercel.app/"}})
 
 @app.route('/')
 def index():
   return render_template('index.html')
 
 @app.route('/print')
+@cross_origin()
 def print_hello():
   return("hello")
 
