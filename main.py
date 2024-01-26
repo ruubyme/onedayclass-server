f68739b19f41c4e138020d25093f8d13a8416397
ruubyme <wngml1741@naver.com>
2024-01-26 10:41:21 +0900

    test

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
   return render_template('index.html')
 
+@app.route('/print')
+def print_hello():
+  return("hello")
+
 if __name__ == '__main__':
   app.run(port=5000)
